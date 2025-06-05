10f74f44fda36760f66d625535639e61f6ae040f
Ethan Schoonbee <ethan.schoonbee@entelect.co.za>
2025-06-05 13:02:29 +0200

    Edited image urls

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -143,10 +143,10 @@ export default function IDEPortfolio() {
 
   // Array of overlay image URLs
   const costumeOverlays = [
-    "/assets/static/images/profile/headshot_1.jpg", // original photo
-    "/assets/images/static/profile/headshot_overlay_cowboy.jpg",
-    "/assets/images/static/profile/headshot_overlay_sunglasses.jpg",
-    "/assets/images/static/profile/headshot_overlay_princess.jpg",
+    "/assets/images/profile/headshot_1.jpg", // original photo
+    "/assets/images/profile/headshot_overlay_cowboy.jpg",
+    "/assets/images/profile/headshot_overlay_sunglasses.jpg",
+    "/assets/images/profile/headshot_overlay_princess.jpg",
   ];
 
   // navigate through costume overlays
@@ -173,7 +173,7 @@ export default function IDEPortfolio() {
           <header className="mb-3">
             <div className="flex flex-col md:flex-row items-center md:items-start space-y-4 md:space-y-0 md:space-x-4 mb-7 mt-4">
               <Image
-                  src="/assets/static/images/profile/headshot_1.jpg"
+                  src="/assets/images/profile/headshot_1.jpg"
                   alt="Ethan Shoonbee"
                   width={100}
                   height={100}
